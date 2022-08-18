3d5547fb00e2a97114cf8a808478ea6388bfd8f5
Joshua Raimer <joshraimer@gmail.com>
2022-08-18 11:35:42 -0700

    changed link description in app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React! Woo Hoo!
         </a>
       </header>
     </div>
